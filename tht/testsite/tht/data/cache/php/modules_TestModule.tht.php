2655be957aa19fcb62adb6bb99951a33444bb03f
joelesko <joe@joelesko.com>
2018-03-15 10:46:57 -0700

    update testsite

--- tht/testsite/tht/data/cache/php/modules_TestModule.tht.php
+++ tht/testsite/tht/data/cache/php/modules_TestModule.tht.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace tht59f4354d72243;
-\o\Runtime::setNameSpace('modules/TestModule.tht','tht59f4354d72243');
+namespace tht5aaab049499a0;
+\o\Runtime::setNameSpace('modules/TestModule.tht','tht5aaab049499a0');
 
 function u_bare_fun ($u_name)  {
  return \o\Runtime::concat("bare:", $u_name);
