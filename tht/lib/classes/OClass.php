b035fef2d6623340afa4c4d06492f24a96a28470
joelesko <joe@joelesko.com>
2018-06-12 22:30:16 -0700

    Prepend 'z' to OClass meta methods

--- tht/lib/classes/OClass.php
+++ tht/lib/classes/OClass.php
@@ -11,8 +11,8 @@ class OClass {
 
         $this->u_state = OMap::create([]);
 
-        if (method_exists($this, 'u_constructor')) {
-            call_user_func_array([ $this, 'u_constructor' ], $args);
+        if (method_exists($this, 'u_new')) {
+            call_user_func_array([ $this, 'u_new' ], $args);
         }
 
         $this->_fieldsLocked = true;
@@ -27,13 +27,18 @@ class OClass {
 
     function __get ($field) {
 
-        if (method_exists($this, 'u_dynamic_get')) {
-            return $this->u_dynamic_get($field);
+        $field = unu_($field);
+
+        if (method_exists($this, 'u_z_dynamic_get')) {
+            $result = $this->u_z_dynamic_get($field);
+            if ($result->u_ok()) {
+                return $result->u_get();
+            }
         }
 
         $suggestion = '';
-        if (method_exists($this, 'u_suggest_field')) {
-            $suggestion = $this->u_suggest_field(unu_($field));
+        if (method_exists($this, 'u_z_suggest_field')) {
+            $suggestion = $this->u_z_suggest_field(unu_($field));
         }
         $suggest = $suggestion ? " Try: `"  . $suggestion . "`" : '';
 
@@ -42,8 +47,8 @@ class OClass {
 
     function __set ($field, $value) {
 
-        if (method_exists($this, 'u_dynamic_set')) {
-            return $this->u_dynamic_set($field, $value);
+        if (method_exists($this, 'u_z_dynamic_set')) {
+            return $this->u_z_dynamic_set($field, $value);
         }
 
         if ($this->_fieldsLocked) {
@@ -54,13 +59,16 @@ class OClass {
 
     function __call ($method, $args) {
 
-        if (method_exists($this, 'u_dynamic_call')) {
-            return $this->u_dynamic_call(unu_($method), $args);
+        if (method_exists($this, 'u_z_dynamic_call')) {
+            $result = $this->u_z_dynamic_call(unu_($method), v($args));
+            if ($result->u_ok()) {
+                return $result->u_get();
+            }
         }
 
         $suggestion = '';
-        if (method_exists($this, 'u_suggest_method')) {
-            $suggestion = $this->u_suggest_method(unu_($method));
+        if (method_exists($this, 'u_z_suggest_method')) {
+            $suggestion = $this->u_z_suggest_method(unu_($method));
         }
         $suggest = $suggestion ? " Try: `"  . $suggestion . "()`" : '';
 
@@ -69,35 +77,41 @@ class OClass {
         Tht::error("Unknown method `$method` for class `$c`. $suggest");
     }
 
-    function u_call_method($method, $args) {
+    function u_z_call_method($method, $args=[]) {
         $uMethod = u_($method);
-        call_user_func_array([ $this, $uMethod ], $args);
+        return call_user_func_array([ $this, $uMethod ], uv($args));
     }
 
-    function u_set_field($field, $vel) {
+    function u_z_set_field($field, $value) {
         $uField = u_($field);
-        $this->$uField = $val;
+        $this->$uField = $value;
     }
 
-    function u_get_field($field) {
+    function u_z_get_field($field) {
         $uField = u_($field);
         return $this->$uField;
     }
 
-    function u_has_method ($method) {
+    function u_z_has_method ($method) {
         return method_exists($this, u_($method));
     }
 
-    function u_has_field ($field) {
+    function u_z_has_field ($field) {
         return property_exists($this, u_($field));
     }
 
-    function u_fields () {
+    function u_z_fields () {
         $fields = get_object_vars($this);
-        return $this->userDefinedElements(array_keys($fields));
+        $uFields = $this->userDefinedElements(array_keys($fields));
+        $fieldMap = [];
+        foreach ($uFields as $f) {
+            $u = u_($f);
+            $fieldMap[$f] = $this->$u; 
+        }
+        return OMap::create($fieldMap);
     }
 
-    function u_methods () {
+    function u_z_methods () {
         $methods = get_class_methods(get_called_class());
         return $this->userDefinedElements($methods);
     }
