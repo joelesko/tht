282c62f9be3035d3c255488db85707870c685ab4
joelesko <joe@joelesko.com>
2019-02-03 18:46:42 -0800

    add methods: toList, slice

--- tht/lib/classes/OMap.php
+++ tht/lib/classes/OMap.php
@@ -4,8 +4,18 @@ namespace o;
 
 class OMap extends OBag {
 
-    function __toString() {
-        return json_encode($this->__toJson()); 
+    protected $suggestMethod = [
+        'count'   => 'length()',
+        'size'    => 'length()',
+        'empty'   => 'isEmpty()',
+        'delete'  => 'remove()',
+    ];
+
+    public function jsonSerialize() {
+        if (!count($this->val)) {
+            return '{EMPTY_MAP}';
+        }
+        return $this->val;
     }
 
     static function create ($ary) {
@@ -18,21 +28,25 @@ class OMap extends OBag {
     }
 
     function u_clear() {
+        ARGS('', func_get_args());
         $this->val = OMap::create([]);
         return $this;
     }
 
     function u_copy() {
+        ARGS('', func_get_args());
         // php apparently copies the array when assigned to a new var
         $a = uv($this->val);
         return OMap::create($a);
     }
 
     function u_is_empty () {
+        ARGS('', func_get_args());
         return count(uv($this->val)) === 0;
     }
 
     function u_remove ($k) {
+        ARGS('s', func_get_args());
         $v = isset($this->val[$k]) ? $this->val[$k] : null;
         unset($this->val[$k]);
         if (is_null($v)) {
@@ -46,14 +60,17 @@ class OMap extends OBag {
     }
 
     function u_has_key ($key) {
+        ARGS('s', func_get_args());
 		return isset($this->val[$key]);
     }
 
     function u_has_value ($value) {
+        ARGS('s', func_get_args());
 		return array_search($value, $this->val, true) !== false;
     }
 
     function u_get_key ($value) {
+        ARGS('s', func_get_args());
 		$found = array_search($value, $this->val, true);
         if ($found === false) {
             $v = v($value)->u_limit(20);
@@ -69,12 +86,31 @@ class OMap extends OBag {
         return OList::create(array_keys($this->val));
     }
 
+    function u_to_list () {
+        $out = [];
+        foreach ($this->val as $k => $v) {
+            $out []= $k;
+            $out []= $v;
+        }
+        return OList::create($out);
+    }
+
     function u_reverse () {
 		return OMap::create(array_flip($this->val));
     }
 
     function u_merge ($a2) {
+        ARGS('m', func_get_args());
         return OMap::create(array_merge($this->val, $a2->val));
+    }
+
+    function u_slice ($keys) {
+        ARGS('l', func_get_args());
+        $out = [];
+        foreach ($keys as $k) {
+            $out[$k] = $this[$k];
+        }
+        return OMap::create($out);
     }
 }
 
