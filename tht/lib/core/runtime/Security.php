581295177ad5b4555c90a8ec324206e3648d7943
joelesko <joe@joelesko.com>
2019-04-06 12:27:34 -0700

    remove headers with input data

--- tht/lib/core/runtime/Security.php
+++ tht/lib/core/runtime/Security.php
@@ -112,13 +112,11 @@ class Security {
             }
         }
 
-        // Correct spelling of "referrer"
-        if (isset($headers['referer'])) {
-            $headers['referrer'] = $headers['referer'];
-            unset($headers['referer']);
-        }
-
-        $headers['cookie'] = '(removed)';
+        unset($headers['referer']);
+        unset($headers['cookie']);
+        unset($headers['accept-language']);
+        unset($headers['host']);
+        unset($headers['user-agent']);
 
         return $headers;
     }
@@ -291,7 +289,7 @@ class Security {
         $isTrailingSlash = $pathSize > 1 && $path[$pathSize-1] === '/';
         if (preg_match('/[^a-z0-9\-\/\.]/', $path) || $isTrailingSlash)  {
             Tht::errorLog("Path `$path` is not valid");
-            Tht::module('Web')->u_send_error(404);
+            Tht::module('Response')->u_send_error(404);
         }
     }
 
@@ -371,7 +369,7 @@ class Security {
 
         // logging
         error_reporting(E_ALL);
-        ini_set('display_errors', Tht::isMode('cli') ? '1' : (Tht::getConfig('_leakPhpErrors') ? '1' : '0'));
+        ini_set('display_errors', Tht::isMode('cli') ? '1' : (Tht::getConfig('_coreDevMode') ? '1' : '0'));
         ini_set('display_startup_errors', '1');
         ini_set('log_errors', '0');  // assume we are logging all errors manually
 
@@ -383,32 +381,6 @@ class Security {
         ini_set('max_execution_time', Tht::isMode('cli') ? 0 : intval(Tht::getConfig('maxExecutionTimeSecs')));
         ini_set('max_input_time', intval(Tht::getConfig('maxInputTimeSecs')));
         ini_set('memory_limit', intval(Tht::getConfig('memoryLimitMb')) . "M");
-
-        self::validatePhpIni();
-    }
-
-    static function validatePhpIni() {
-
-        // Configs that are only set in .ini or .htaccess
-        // Trigger an error if PHP is more strict than Tht.
-        $thtMaxPostSize = intval(Tht::getConfig('maxPostSizeMb'));
-        $phpMaxFileSize = intval(ini_get('upload_max_filesize'));
-        $phpMaxPostSize = intval(ini_get('post_max_size'));
-        $thtFileUploads = Tht::getConfig('allowFileUploads');
-        $phpFileUploads = ini_get('file_uploads');
-
-        if ($thtMaxPostSize > $phpMaxFileSize) {
-            Tht::configError("Config `maxPostSizeMb` ($thtMaxPostSize) is larger than php.ini `upload_max_filesize` ($phpMaxFileSize).\n"
-                . "You will want to edit php.ini so they match.");
-        }
-        if ($thtMaxPostSize > $phpMaxPostSize) {
-            Tht::configError("Config `maxPostSizeMb` ($thtMaxPostSize) is larger than php.ini `post_max_size` ($phpMaxPostSize).\n"
-                . "You will want to edit php.ini so they match.");
-        }
-        if ($thtFileUploads && !$phpFileUploads) {
-            Tht::configError("Config `allowFileUploads` is true, but php.ini `file_uploads` is false.\n"
-                . "You will want to edit php.ini so they match.");
-        }
     }
 
     // Register an un-sandboxed version of File, for internal use.
