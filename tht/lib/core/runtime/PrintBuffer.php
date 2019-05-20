851c2a0c5e04ee501db847b9f3f8eff5a8eb18f3
joelesko <joe@joelesko.com>
2019-05-20 10:47:30 -0700

    update font call

--- tht/lib/core/runtime/PrintBuffer.php
+++ tht/lib/core/runtime/PrintBuffer.php
@@ -21,7 +21,7 @@ class PrintBuffer {
         $zIndex = 100000;
 
         echo "<style>\n";
-        echo ".tht-print { white-space: pre; border: 0; border-left: solid 16px #60adff; padding: 4px 32px; margin: 4px 0 0;  font-family: " . Tht::module('Css')->u_monospace_font() ."; }\n";
+        echo ".tht-print { white-space: pre; border: 0; border-left: solid 8px #60adff; padding: 4px 32px; margin: 4px 0 0;  font-family: " . Tht::module('Css')->u_font('monospace') ."; }\n";
         echo ".tht-print-panel { position: fixed; top: 0; left: 0; z-index: $zIndex; width: 100%; padding: 24px 32px 24px; font-size: 18px; background-color: rgba(255,255,255,0.98);  -webkit-font-smoothing: antialiased; color: #222; box-shadow: 0 4px 4px rgba(0,0,0,0.15); max-height: 400px; overflow: auto;  }\n";
         echo "</style>\n";
 
