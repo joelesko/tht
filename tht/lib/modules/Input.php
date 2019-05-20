c4678e29e40533ceaffa2242d239c495f2ac2a84
joelesko <joe@joelesko.com>
2019-05-20 10:51:01 -0700

    final API

--- tht/lib/modules/Input.php
+++ tht/lib/modules/Input.php
@@ -3,7 +3,6 @@
 namespace o;
 
 require_once('helpers/RequestData.php');
-require_once('helpers/InputValidator.php');
 
 class u_Input extends StdModule {
 
@@ -33,17 +32,20 @@ class u_Input extends StdModule {
 
     // Get Single Field
 
-    function u_get($name, $sRules='id') {
+    function u_get($name, $sRules='') {
+        ARGS('ss', func_get_args());
         $getter = new u_RequestData ('get');
         return $getter->field($name, $sRules)['value'];
     }
 
-    function u_post($name, $sRules='id') {
+    function u_post($name, $sRules='') {
+        ARGS('ss', func_get_args());
         $getter = new u_RequestData ('post');
         return $getter->field($name, $sRules)['value'];
     }
 
-    function u_rest($method, $fieldName, $sRules='id') {
+    function u_remote($method, $fieldName, $sRules='') {
+        ARGS('sss', func_get_args());
         $getter = new u_RequestData ($method);
         return $getter->field($fieldName, $sRules)['value'];
     }
@@ -52,34 +54,46 @@ class u_Input extends StdModule {
     // Get Map of Fields
 
     function u_get_all($rules) {
-        $getter = new u_RequestData ('get', $rules);
-        return $getter->fields();
+        ARGS('m', func_get_args());
+        $getter = new u_RequestData ('get');
+        return $getter->fields($rules);
     }
 
-    function u_post_all($rules=null) {
-        $getter = new u_RequestData ('post', $rules);
-        return $getter->fields();
+    function u_post_all($rules) {
+        ARGS('m', func_get_args());
+        $getter = new u_RequestData ('post');
+        return $getter->fields($rules);
     }
 
-    function u_rest_all($method, $rules=null) {
-        $getter = new u_RequestData ($method, $rules);
-        return $getter->fields();
+    function u_remote_all($method, $rules) {
+        ARGS('sm', func_get_args());
+        $getter = new u_RequestData ($method, true);
+        return $getter->fields($rules);
+    }
+
+    function u_log_all($method) {
+        ARGS('s', func_get_args());
+        $all = OMap::create(Tht::getPhpGlobal($method, '*'));
+        Tht::module('File')->u_log($all);
     }
 
 
     // Meta-Getters
 
     function u_fields($method) {
+        ARGS('s', func_get_args());
         $getter = new u_RequestData ($method);
         return OList::create($getter->fieldNames());
     }
 
     function u_has_field($method, $fieldName) {
+        ARGS('ss', func_get_args());
         $getter = new u_RequestData ($method);
-        return OList::create($getter->hasField($fieldName));
+        return $getter->hasField($fieldName);
     }
 
     function u_validate($fieldName, $val, $rules) {
+        ARGS('s*s', func_get_args());
         $validator = new u_InputValidator ();
         $validated = $validator->validateField($fieldName, $val, $rules);
 
