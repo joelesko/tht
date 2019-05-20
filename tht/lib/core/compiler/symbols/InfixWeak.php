f7be3549bfd812096d7dd87819b409070eee99a7
joelesko <joe@joelesko.com>
2019-05-20 10:57:55 -0700

    rename InfixRight to InfixWeak

---
+++ tht/lib/core/compiler/symbols/InfixWeak.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace o;
+
+// Infix, but with a lower binding power
+class S_InfixWeak extends Symbol {
+    var $type = SymbolType::INFIX;
+    var $isAssignment = false;
+
+    function asInner ($p, $left) {
+        $p->next();
+        if ($this->isAssignment && $p->expressionDepth >= 2 && !$p->allowAssignmentExpression) {
+            $tip = $this->token[TOKEN_VALUE] == '=' ? "Did you mean `==`?" : '';
+            $p->error("Assignment can not be used as an expression.  $tip", $this->token);
+        }
+        $this->space(' = ');
+        $this->setKids([$left, $p->parseExpression($this->bindingPower - 1)]);
+        return $this;
+    }
+}
+
+class S_BitShift extends S_InfixWeak {
+    // e.g. +>, +<
+    var $type = SymbolType::BITSHIFT;
+    var $bindingPower = 45;
+}
+
+class S_ValGate extends S_InfixWeak {
+    // e.g. &&:, ||:
+    var $type = SymbolType::VALGATE;
+    var $bindingPower = 41;
+}
+
+class S_Compare extends S_InfixWeak {
+    // e.g. !=, ==
+    var $bindingPower = 40;
+}
+
+class S_Bitwise extends S_InfixWeak {
+    // e.g. +&, +|
+    var $type = SymbolType::BITWISE;
+    var $bindingPower = 30;
+}
+
+class S_Logic extends S_InfixWeak {
+    // e.g. ||, &&
+    var $bindingPower = 20;
+}
+
+class S_Assign extends S_InfixWeak {
+    // =, +=, etc.
+    var $type = SymbolType::ASSIGN;
+    var $bindingPower = 10;
+    var $isAssignment = true;
+}
