04dddf1d3c50f84f0d8f312d7fb155caa2faff4c
joelesko <joe@joelesko.com>
2019-06-09 20:17:40 -0700

    move isa to OClass

--- tht/lib/classes/OVar.php
+++ tht/lib/classes/OVar.php
@@ -9,15 +9,5 @@ class OVar extends OClass implements \JsonSerializable {
     public function jsonSerialize() {
         return $this->val;
     }
-
-    static function isa ($s) {
-        if (is_object($s)) {
-            $called = get_called_class();
-            if ($called === get_class($s) || $called === get_parent_class($s)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 
