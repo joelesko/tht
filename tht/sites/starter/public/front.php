b428a8009bbb6de15c498982c62cc48be960987d
joelesko <joe@joelesko.com>
2022-12-03 22:03:16 -0600

    fix system path for homebrew installed php

--- tht/sites/starter/public/front.php
+++ tht/sites/starter/public/front.php
@@ -2,7 +2,6 @@
 
 // Front Controller
 // This routes all incoming requests to the THT app.
-
-$thtRuntime = __DIR__ . '/../system/tht/run/tht.php';
+$thtRuntime = dirname(__FILE__) . '/../system/tht/run/tht.php';
 
 return require_once($thtRuntime);
